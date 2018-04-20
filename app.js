c673a983b9733042f8400a4f38022775a46cdc7b
timhamrouge <t.hamrouge@gmail.com>
2018-04-20 13:41:03 +0100

    adding access

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const apiRouter = require("./routes/api");
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 mongoose.connect(DB_URL).then(() => {
   console.log(`connected to ${DB_URL}`);
 });
